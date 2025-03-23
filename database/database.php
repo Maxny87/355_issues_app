d581cd10ddf18b55e095743bc8582afd0c7e0791
Max Yaw <mnyyaw71@gmail.com>
2025-03-22 23:13:37 -0400

    Stop tracking db directory and add to .gitignore

--- database/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$db_name = "issues_app";
-$db_user = "root";
-$db_pass = "";
-$server = "127.0.0.1";
-$connstring = "mysql:host=" . $server . ";port=3307;dbname=" . $db_name . ";charset=utf8mb4;";
-
